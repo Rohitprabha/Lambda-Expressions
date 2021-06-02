15d696c9930bc1b32aa11b7cc9abe62c23297076
Rohitprabha <67499119+Rohitprabha@users.noreply.github.com>
2021-06-02 11:00:01 +0530

    Add files via upload

---
+++ Example 2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lambda_Expressions_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int> { 1, 2, 3, 4 };
+            List<int> resultUsingExpressionLambda = list.FindAll(p => p < 3);
+            Console.WriteLine("Using expressions lambda: ");
+            foreach (int item in resultUsingExpressionLambda)
+            {
+                Console.WriteLine(item);
+            }
+            List<int> resultUsingStatementLambda = list.FindAll(p =>
+            {
+                return p < 3;
+            });
+            Console.WriteLine("Using statements lambda: ");
+            foreach (int item in resultUsingStatementLambda)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
