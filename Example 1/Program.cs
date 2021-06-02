d3b6be2f39e1755f7a7e70e3a7f6e29e7e5941a2
Rohitprabha <67499119+Rohitprabha@users.noreply.github.com>
2021-06-02 10:58:16 +0530

    Add files via upload

---
+++ Example 1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Lambda_Expressions
+{
+    public delegate string MessageDelegate(string name);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MessageDelegate obj = (name) =>                         //Lambda Expression
+            {
+                return "Hai " + name;
+            };
+            string str = obj.Invoke("Rohit");
+            Console.WriteLine(str);
+        }
+    }
+}
